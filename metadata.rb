55b8757cf1d484be208076921c8aa0f2fbb3cf52
Jonnie <swagopopotamus@gmail.com>
2015-01-30 14:54:18 -0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Joshua Timberman'
 maintainer_email 'cookbooks@housepub.org'
 license          'Apache 2.0'
 description      'Installs/Configures samba'
-version          '0.12.1'
+version          '0.12.2'
 
 recipe 'samba::default', 'Includes the samba::client recipe'
 recipe 'samba::client', 'Installs smbclient package'
